072ad033204349a66b9aeba5d4c4c8cd67edd2a9
anubiscapstone <anubiscapstone@gmail.com>
2014-09-03 19:39:51 -0400

    @author Matt
    * Removed dummy .c file
    * Created empty VS Win forms project "AnubisClient"

---
+++ AnubisClient/AnubisClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace AnubisClient
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form());
+        }
+    }
+}
