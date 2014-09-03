e4b9b75f9ce350534f8f9685496fce4a0f527b73
anubiscapstone <anubiscapstone@gmail.com>
2014-09-03 14:59:16 -0400

    sdklfjh
    sdfsdfsdf

--- main.c
+++ main.c
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
+
 #include <stdio.h>
 
 main(void) {
 	printf("Holla, world!");
 	return 0;
 }
-=======
-#include <AI>
-#include <Oculus_facebook_drivers>
-#include <skynet>
 
-int main {
-    do_nothing();
-}
->>>>>>> 40ebb5fb0bcdf70a9a64ea7b2f781aceeecd5423
