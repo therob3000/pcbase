4ae87bcc2f35ed9edadbdb7983aae759f759810c
ncTsmith <t-smith.1@onu.edu>
2014-10-24 15:15:58 -0400

    *Continuation of new Form Framework

--- AnubisClient/AnubisClient/ClientForm.Designer.cs
+++ AnubisClient/AnubisClient/ClientForm.Designer.cs
@@ -29,25 +29,50 @@
         private void InitializeComponent()
         {
             this.NetCommWorker = new System.ComponentModel.BackgroundWorker();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.ts_Windows = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ts_Windows});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(802, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // ts_Windows
+            // 
+            this.ts_Windows.Name = "ts_Windows";
+            this.ts_Windows.Size = new System.Drawing.Size(56, 22);
+            this.ts_Windows.Text = "Windows";
             // 
             // ClientForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(802, 524);
+            this.Controls.Add(this.toolStrip1);
             this.Name = "ClientForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ClientForm";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ClientForm_FormClosing);
             this.Load += new System.EventHandler(this.ClientForm_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.ComponentModel.BackgroundWorker NetCommWorker;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel ts_Windows;
 
 
     }
---
+++ AnubisClient/AnubisClient/CommandView.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AnubisClient
+{
+    public partial class CommandView : Form
+    {
+        public CommandView()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_UpdateGrid_Click(object sender, EventArgs e)
+        {
+            string[] commandList = CommandBuilder.GetCurrentCommandArray();
+            int i = 0;
+            foreach (string channel in commandList)
+            {
+                lbx_CommandList.Items.Add("Channel 1 : " + channel);
+                i++;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string[] commandList = CommandBuilder.GetCurrentCommandArray();
+            int i = 0;
+            foreach (string channel in commandList)
+            {
+                lbx_CommandList.Items.Add("Channel 1 : " + channel);
+                i++;
+            }
+        }
+
+    }
+}
