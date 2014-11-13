151c1f2ea28ace634a31be7c3b2a43e8442c67b6
Matt <m-hunter.2@onu.edu>
2014-11-13 01:00:17 -0500

    * Implemented Sock

--- AnubisClient/AnubisClient/Comm_old/Sock.cs
+++ AnubisClient/AnubisClient/Comm_old/Sock.cs
@@ -1,90 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net;
-using System.Net.Sockets;
+﻿
 
-namespace AnubisClient
-{
-    public class Sock
-    {
-        private Socket sock;
-        private int _port;
-        private string _rmtHost;
-
-        public string rmtHost
-        {
-            get
-            {
-                return _rmtHost;
-            }
-        }
-
-        public int port
-        {
-            get
-            {
-                return _port;
-            }
-        }
-
-        public Sock(int Port)
-        {
-            sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            _rmtHost = "";
-            _port = Port;
-            sock.ReceiveTimeout = 300000;
-            sock.Bind(new IPEndPoint(0, _port));
-            
-        }
-
-        public Sock(string RemoteHost, int Port)
-        {
-            sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            _rmtHost = RemoteHost;
-            _port = Port;
-            sock.ReceiveTimeout = 300000;
-            IPAddress[] ipas = Dns.GetHostAddresses(_rmtHost);
-            sock.Connect(ipas[ipas.Length - 1], _port);
-        }
-
-        private Sock(Socket sockt, string RemoteHost, int Port)
-        {
-            this.sock = sockt;
-            _rmtHost = RemoteHost;
-            _port = Port;
-        }
-
-        public void Listen()
-        {
-            sock.Listen(10);
-        }
-
-        public Sock Accept()
-        {
-            return new Sock(sock.Accept(), rmtHost, port);
-        }
-
-        public void sendline(string line)
-        {
-            sock.Send(ASCIIEncoding.ASCII.GetBytes(line + "\n"));
-        }
-
-        public string readline()
-        {
-            string message = "";
-            while (true)
-            {
-                byte[] bytes = new byte[1024];
-                int bytesrec = sock.Receive(bytes);
-                message += Encoding.ASCII.GetString(bytes, 0, bytesrec);
-                if (message.IndexOf("\n") > 1) break;
-            }
-            message = message.Substring(0, message.Length - 1);
-            return message;
-        }
-
+        // put somewhere else
         public string readData(string Paramater)
         {
             string r_message = "";
@@ -109,6 +25,7 @@ namespace AnubisClient
             }
         }
 
+        // put somewhere else
         public string readCommand()
         {
             string r_message = "";
@@ -132,10 +49,3 @@ namespace AnubisClient
                 return "Network Error";
             }
         }
-
-        public void close()
-        {
-            sock.Close();
-        }
-    }
-}
---
+++ AnubisClient/AnubisClient/Sock.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace AnubisClient {
+	public class Sock {
+		private Socket sock;
+		private string _rmtHost;
+		private int _port;
+
+		public string rmtHost {
+			get {
+				return _rmtHost;
+			}
+		}
+
+		public int port {
+			get {
+				return _port;
+			}
+		}
+
+		public Sock(int port) { // Server Constructor
+			sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			_rmtHost = "";
+			_port = port;
+			sock.ReceiveTimeout = 300000;
+			sock.Bind(new IPEndPoint(0, _port));
+		}
+
+		public Sock(string rmtHost, int port) { // Client Constructor
+			sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			_rmtHost = rmtHost;
+			_port = port;
+			sock.ReceiveTimeout = 300000;
+			IPAddress[] ipas = Dns.GetHostAddresses(_rmtHost);
+			sock.Connect(ipas[ipas.Length - 1], _port);
+		}
+
+		private Sock(Socket sock, string rmtHost, int port) { // Accept Constructor
+			this.sock = sock;
+			_rmtHost = rmtHost;
+			_port = port;
+		}
+
+		public void listen() {
+			sock.Listen(10);
+		}
+
+		public Sock accept() {
+			return new Sock(sock.Accept(), rmtHost, port);
+		}
+
+		public void sendline(string line) {
+			sock.Send(ASCIIEncoding.ASCII.GetBytes(line + "\n")); // Append a new-line when sending
+		}
+
+		public string readline() { // blocks
+			string message = "";
+			while (true) {
+				byte[] bytes = new byte[1024];
+				int bytesrec = sock.Receive(bytes);
+				message += Encoding.ASCII.GetString(bytes, 0, bytesrec);
+				if (message.IndexOf("\n") > 1) break; // Use new-line to identify end-of-message
+			}
+			message = message.Substring(0, message.Length - 1); // Strip new-line when receiving
+			return message;
+		}
+
+		public void close() {
+			sock.Close();
+		}
+	}
+}
